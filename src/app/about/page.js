b5b69b03d145d9a8184b417a1545cd9e5f76cc6a
shijith <shijithsanthosh@gmail.com>
2023-09-01 12:25:36 +0530

    Change in portrait name

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -57,7 +57,7 @@ const people = {
     },
     {
       name: "Ayan Mukhopadhyay",
-      image: "/assets/portraits/ayan.jpg",
+      image: "/assets/portraits/mukhopadhyay.jpg",
       univ: "Vanderbilt University",
     },
   ],
