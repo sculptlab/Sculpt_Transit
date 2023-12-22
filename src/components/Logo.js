d96ad72d854a0fcc67cfaaa913a9ee56d788de06
shijith <shijithsanthosh@gmail.com>
2023-12-22 12:54:12 +0530

    Add hyperlink to homepage on dashboard logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,15 @@
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import styles from "src/css/page.module.css";
 
 function Logo() {
   return (
     <div>
-      <div className={styles.logo}>
-        <img src={"/assets/SCULPT_logo.png"} alt="SCULPT logo" />
-      </div>
+      <Link href="/">
+        <div className={styles.logo}>
+          <img src={"/assets/SCULPT_logo.png"} alt="SCULPT logo" />
+        </div>
+      </Link>
     </div>
   );
 }
